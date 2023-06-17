38456cfea3c3599b5f7fad719451df003b4c4ee8
inbumbaek <inbumbaek@gmail.com>
2023-06-16 21:50:25 -0700

    user context

---
+++ src/context/UserContext.js
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+export const userContext = createContext()
+
+export const UserProvider = (props) => {
+    const [userList, setUserList] = useState([])
+    return (
+        <userContext.Provider value={{
+            userList,
+            setUserList
+        }}>
+    )
+}
